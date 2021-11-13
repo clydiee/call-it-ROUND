4dbe0a7c9824ea4a9d5775ffc7855d511de02cf7
clydie <45523435+clydiee@users.noreply.github.com>
2021-11-13 20:47:57 +0000

    UH i uh uh uh uh uh
    um well i uh

---
+++ mouselook.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouselook : MonoBehaviour
+{
+    //i am literally GOD at coding
+
+    public float mouseSensitivity = 900f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
